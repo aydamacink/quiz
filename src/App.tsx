095c0e0c0d75c522e51b62149f2d66b53f8de2af
Ayda Macink <dayanamick@gmail.com>
2025-12-11 10:56:23 +0100

    initial commit: setting up the project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "system-ui, sans-serif" }}>
+      <h1>Crypto Jargon Quiz</h1>
+      <p>Hi, future on-chain legend. Ready to test your crypto vocab?</p>
+    </main>
+  );
+}
+
+export default App;
